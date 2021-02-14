2c9b3df3c37f8a5402cbbef6dd881a5a11dac6c6
Nithin Veer Reddy Kankanti <nkankanti@truu.ai>
2021-02-13 23:18:16 -0700

    Update uftp_client.c

--- Assignment-1Solution/client/uftp_client.c
+++ Assignment-1Solution/client/uftp_client.c
@@ -11,6 +11,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #define BUFSIZE 1024
+#define MAX_ATTEMTS 100
 
 /*
  * error - wrapper for perror
@@ -36,9 +37,8 @@ int main(int argc, char **argv) {
     int sockfd, portno, n, m, recvlen, serverlen;
     struct sockaddr_in serveraddr;
     struct hostent *server;
-    char *hostname, buf[BUFSIZE], inp[BUFSIZE],command[10] = {0}, filename[20] = {0};
+    char *hostname, buf[BUFSIZE], inp[BUFSIZE],command[10] = {0}, filename[20] = {0}, key = 10;
     packet_info packet;
-    char key = 10;
     memset(&packet, 0, sizeof(packet));
     long int ACK = 0;
     struct timeval timeout = {0, 700000};
@@ -57,7 +57,7 @@ int main(int argc, char **argv) {
     if (sockfd < 0)
         error("ERROR opening socket");
 
-    /* gethostbyname: get the server's DNS entry */
+    /* get host by name: get the server's DNS entry */
     server = gethostbyname(hostname);
     if (server == NULL) {
         fprintf(stderr, "ERROR, no such host as %s\n", hostname);
@@ -73,36 +73,38 @@ int main(int argc, char **argv) {
           (char *) &serveraddr.sin_addr.s_addr, server->h_length);
     serveraddr.sin_port = htons(portno);
 
+    //Make a connection to the server
     connect(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr));
-
+    //We continue to keep the connection alive until an exit command is received.
     while (1) {
-        // Command Menu
-        printf("\n Enter any of the Following Commands  \n 1. get [file_name] \n 2. put [file_name] \n 3. delete [file_name] \n 4. ls \n 5. md \n 5. exit \n");
+        // Helps the user to see what all commands are accepted with arguments.
+        printf("\nEnter any of the Following Commands  \n 1. get [file_name] \n 2. put [file_name] \n 3. delete [file_name] \n 4. ls \n 5. md \n 5. exit \n");
 
         memset(inp, 0, 50);
         memset(command, 0, 10);
         memset(filename, 0, 20);
-        // Recieve the command
+        // Receives the command as user inputs
         scanf(" %[^\n]%*c", inp);
         sendto(sockfd, inp, strlen(inp), 0, (struct sockaddr *) &serveraddr, sizeof(serveraddr));
         sscanf(inp, "%s %s", command, filename);
-        // Put the file into the server
-        if (((strcmp(command, "put") == 0)) && (*filename != '\0')) // Check if the filename is empty or not
+
+
+        /* Implementation of PUT commands */
+        if (((strcmp(command, "put") == 0)) && (*filename != '\0')) // Check if the filename is empty or not for a PUT command
         {
             long int num_packets = 0, dropped = 0;
             fptr = fopen(filename, "rb"); // open the file to read
-
+            /* Set the timeout to receive an acknowledgment */
             setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *) &timeout,
-                       sizeof(struct timeval)); // Set the timeout for Acknowlegment
+                       sizeof(struct timeval));
 
-            if ((fptr == NULL)) // Check if the filename was right or wrong
+            if ((fptr == NULL)) // Check to see if file information is correct
             {
                 printf("Wrong File name \n");
-                // Send filesize as zero so that server will know that filename was wrong
-                sendto(sockfd, &num_packets, sizeof(num_packets), 0, (struct sockaddr *) &serveraddr,
-                       sizeof(serveraddr));
-                recvfrom(sockfd, &num_packets, sizeof(num_packets), 0, (struct sockaddr *) &serveraddr,
-                         &addrlen); // Recieve ACK for number of packets
+                /* We send a null size file so that the server is aware of the malfunctioned file */
+                sendto(sockfd, &num_packets, sizeof(num_packets), 0, (struct sockaddr *) &serveraddr, sizeof(serveraddr));
+                /* Receive ACK for number of packets */
+                recvfrom(sockfd, &num_packets, sizeof(num_packets), 0, (struct sockaddr *) &serveraddr, &addrlen);
             } else {
 
                 int counter = 0, timeout_flag = 0;
@@ -112,55 +114,66 @@ int main(int argc, char **argv) {
                 memset(&packet, 0, sizeof(packet));
 
                 printf("File Size - %ld \n", file_size);
-                num_packets = (file_size / BUFSIZE) + 1; // Number of packets to send
+                /* Calculating the Number of packets to be sent to Server
+                 * This way we can keep track of each packet even in case of a packet loss
+                 * We retry the packet loss again in case we dont receive the acknowledgment
+                 */
+                num_packets = (file_size / BUFSIZE) + 1;
 
-                // Send the number of packets to the reciever
-                sendto(sockfd, &(num_packets), sizeof(num_packets), 0, (struct sockaddr *) &serveraddr,
-                       sizeof(serveraddr));
-                // wait for ack and if timeout then resend
+                /* Start sending the packets to the server/receiver */
+                sendto(sockfd, &(num_packets), sizeof(num_packets), 0, (struct sockaddr *) &serveraddr, sizeof(serveraddr));
+
+                /* Continue to wait until the timeout incase of no show of acknowledgement*/
                 while (recvfrom(sockfd, &ACK, sizeof(ACK), 0, (struct sockaddr *) &serveraddr, &addrlen) < 0) {
 
-                    sendto(sockfd, &(num_packets), sizeof(num_packets), 0, (struct sockaddr *) &serveraddr,
-                           sizeof(serveraddr));
+                    sendto(sockfd, &(num_packets), sizeof(num_packets), 0, (struct sockaddr *) &serveraddr,sizeof(serveraddr));
                     counter++;
-                    // Even after 100 tries if the server fails then give connection timeout
-                    if (counter == 100) {
+
+                    /* We attempt to retry for the MAX_ATTEMTS times.
+                     * If it fails then client throws a connection timeout
+                     */
+                    if (counter == MAX_ATTEMTS) {
                         timeout_flag = 1;
-                        printf("File Not Sent - Connection Timeout\n");
+                        printf("File can't be sent  -  Connection Timeout\n");
                         break;
                     }
                 }
 
                 printf("packets - %ld \n", num_packets);
-                // Send the data
+                /* Sending the actual packets/data */
                 long int i, j;
                 for (i = 0; i < num_packets; i++) {
                     memset(&packet, 0, sizeof(packet));
                     packet.length = fread(packet.p, 1, BUFSIZE, fptr), counter = 0;
                     ACK = 0;
                     packet.ID = i;
-                    // Encrypt the data by Xoring
                     for ( j = 0; j < packet.length; j++) {
                         packet.p[j] ^= key;
                     }
 
-                    // Send the encrypted data
+                    /* Send the encrypted data */
                     sendto(sockfd, &packet, sizeof(packet), 0, (struct sockaddr *) &serveraddr, sizeof(serveraddr));
 
-                    // If timeout flag is sent then give connection timeout
+                    /* Throw a connection timeout incase of a timeout flage */
                     if (timeout_flag) {
                         printf("File Not Sent - Connection Timeout\n");
                         break;
                     }
-                    // recieve ACK from the reciever
+
+                    /* receive acknowledgement from the server/receiver */
                     recvfrom(sockfd, &ACK, sizeof(ACK), 0, (struct sockaddr *) &serveraddr, &addrlen);
-                    // Keep re sending packets until ack for current packet is recieved
+
+                    /* Send the packets to the receiver
+                     * Retry sending the packet until acknowledgement is received
+                     * Retry for at a max of MAX_ATTEMTS
+                     * Appropriate print statements are provided for easy understanding
+                     */
                     while ((ACK != packet.ID)) {
                         sendto(sockfd, &packet, sizeof(packet), 0, (struct sockaddr *) &serveraddr, sizeof(serveraddr));
                         recvfrom(sockfd, &ACK, sizeof(ACK), 0, (struct sockaddr *) &serveraddr, &addrlen);
                         printf("\t\tPacket no %ld dropped, Total - %ld \n", packet.ID, ++dropped);
                         counter++;
-                        if (counter == 100) {
+                        if (counter == MAX_ATTEMTS) {
                             timeout_flag = 1;
                             break;
                         }
@@ -174,54 +187,59 @@ int main(int argc, char **argv) {
                 }
 
             }
-            // Remove the timeout for the recvfrom
-            struct timeval timeout = {
-                    0,
-                    0
-            };
+            /* Nullifying the timeout*/
+            struct timeval timeout = { 0,0 };
             setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *) &timeout, sizeof(struct timeval));
 
         }
 
-            // client
+        /* Implementation of GET command */
         else if ((strcmp(command, "get") == 0) && (*filename != '\0')) {
 
             long int num_packets = 0;
-            // Recieve the number of packets of the file
+            /* Receive the number of expected packets from the server*/
             recvfrom(sockfd, &num_packets, sizeof(num_packets), 0, (struct sockaddr *) &serveraddr, &addrlen);
-            //Server  ACK
+
+            /* Acknowledgement from the server */
             sendto(sockfd, &num_packets, sizeof(num_packets), 0, (struct sockaddr *) &serveraddr, addrlen);
 
             printf("Packets expected - %ld \n", num_packets);
 
+
+            /* Null check */
             if (num_packets == 0) {
-                printf("Empty File - Not Created \n"); // if 0 packets are recieved that means file can not be created
-            } else if (num_packets > 0) {
+                printf("Empty File - Not Created \n");
+            }
+            else if (num_packets > 0) {
                 int total_bytes = 0;
                 long int i, j;
-                // Open the new file to write
+                /* Create a new file to write */
                 fptr = fopen(filename, "w");
-                // Write the number of packets to file
+
+                /* Write the contents of the packets into the file */
                 for (i = 0; i < num_packets; i++) {
-                    // clear the recieving buffer before writing
+                    /* Emptying the packet buffer*/
                     memset(&packet, 0, sizeof(packet));
-                    // recieve the packet
+
+                    /* Receive the packet */
                     recvlen = recvfrom(sockfd, &packet, sizeof(packet), 0, (struct sockaddr *) &serveraddr, &addrlen);
 
-                    // send acknowledgement
+                    /* send acknowledgement */
                     sendto(sockfd, &(packet.ID), sizeof(packet.ID), 0, (struct sockaddr *) &serveraddr, addrlen);
 
-                    if (packet.ID <
-                        i) // If repeated paxket is recived then discard it and reduce the for loop count of recieved packet
+                    /* If same packet is received, discard the same
+                     * This will not put the program in an infinite loop
+                     */
+                    if (packet.ID < i)
                     {
                         i--;
                         printf("Same Packet %ld Discarded \n", packet.ID);
                     } else {
-                        // Decrypt the data
+
                         for (j = 0; j < packet.length; j++) {
                             packet.p[j] ^= key;
                         }
-                        // Write the data to the file
+                        /* write the packet to the file */
                         fwrite(packet.p, 1, packet.length, fptr);
                         printf("Packet number = %ld Recieved Size %ld  \n", packet.ID, packet.length);
                         total_bytes += packet.length;
@@ -229,19 +247,13 @@ int main(int argc, char **argv) {
 
                 }
                 int k ;
-                for (k = 0; k < 100; k++) {
-                    struct timeval tout = {
-                            0,
-                            800
-                    };
+                for (k = 0; k < MAX_ATTEMTS; k++) {
+                    struct timeval tout = {0, 800 };
                     setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *) &tout, sizeof(struct timeval));
-
                     recvfrom(sockfd, &packet, sizeof(packet), 0, (struct sockaddr *) &serveraddr, &addrlen);
-
                     tout.tv_sec = 0;
                     tout.tv_usec = 0;
                     setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *) &tout, sizeof(struct timeval));
-
                 }
                 printf("Total bytes - %d\n", total_bytes);
                 fclose(fptr);
@@ -249,22 +261,24 @@ int main(int argc, char **argv) {
 
         }
 
-            // client - delete command
+        /* Implementation of DELETE command */
         else if ((strcmp(command, "delete") == 0) && (*filename != '\0')) {
             char ret[50];
             memset(ret, 0, 50);
-            // Print the server reply for delete command
+            /* Receive the delete response from the server */
             recvfrom(sockfd, ret, 50, 0, (struct sockaddr *) &serveraddr, &addrlen);
             printf(" %s \n", ret);
         }
-            // Get the md5sum for the file on the client side
+
+        /* Implementation of MD5SUM  command (Optional)*/
         else if ((strcmp(command, "md") == 0) && (*filename != '\0')) {
             char cmd[50] = "md5sum ";
             strcat(cmd, filename);
             printf("\n md5sum of %s on Client Side \n\n\t", filename);
             system(cmd);
         }
-            // get the list of files on the server side and print it
+
+        /* Implementation of LS command */
         else if (strcmp(command, "ls") == 0) {
             char filelist[200] = {
                     0
@@ -273,14 +287,16 @@ int main(int argc, char **argv) {
             recvfrom(sockfd, filelist, 200, 0, (struct sockaddr *) &serveraddr, &addrlen);
             printf("\n \nList of files is: \n%s \n", filelist);
 
-        } else if (strcmp(command, "exit") == 0) {
+        }
+
+        /* Implementation of EXIT command */
+        else if (strcmp(command, "exit") == 0) {
             return 0;
         }
-            // Error in the command or filename
+
+        /* Implementation of other command */
         else {
-
             printf("Error (Wrong Command or Wrong filename) \n");
-
         }
     }
 
